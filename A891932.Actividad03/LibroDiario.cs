ccbd38ec95bead02beb107887d1a390c1455134e
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-04 19:33:15 -0300

    Creadas las funciones a desarrollar

---
+++ A891932.Actividad03/LibroDiario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891932.Actividad03
+{
+    static class LibroDiario
+    {
+        // Pregunta la ubicacion del plan de cuentas e importa las cuentas.
+        // Pregunta si queremos abrir un libro diario existente o crear uno nuevo.
+        internal static void Iniciar()
+        {
+
+        }
+
+        // Crear nuevos asientos (NroAsiento|Fecha|CodigoCuenta|Debe|Haber)
+        internal static void IngresarAsiento()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Imprime los asientos que posee el libro diario actualmente
+        internal static void VerAsientos()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Imprime el plan de cuentas importado en Iniciar()
+        internal static void VerPlanDeCuentas()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Guarda el libro diario actual
+        internal static void Guardar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
