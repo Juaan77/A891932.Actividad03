5c3e1ed49b0286286584c52e9e9fccc0b0ef4510
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-04 18:46:47 -0300

    Agregado validador de numeros positivos

---
+++ A891932.Actividad03/Validadores.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891932.Actividad03
+{
+    class Validadores
+    {
+        private static float Numeros(string textoAImprimir)
+        {
+            float numero;
+            bool completo = false;
+
+            do
+            {
+                Console.WriteLine(textoAImprimir);
+                
+                if (!float.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine("Debe ingresar un número.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if(numero < 0)
+                    {
+                        Console.WriteLine("El número ingresado debe ser positivo.");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        completo = true;
+                    }                    
+                }
+
+            } while (completo == false);
+
+            return numero;
+        }
+    }
+}
