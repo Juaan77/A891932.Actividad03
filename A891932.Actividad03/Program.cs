9e0b9d8afa88d0663fa66f89cdc3599c81fb6cb2
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-04 19:27:11 -0300

    Esqueleto principal

--- A891932.Actividad03/Program.cs
+++ A891932.Actividad03/Program.cs
@@ -10,6 +10,42 @@ namespace A891932.Actividad03
     {
         static void Main(string[] args)
         {
+            const string menuPrincipal = "I - Ingresar nuevo asiento\n" +
+                "V - Ver asientos ingresados\n" +
+                "P - Ver plan de cuentas\n" +
+                "S - Guardar y salir";
+            string opcionElegida = "";
+
+            Console.WriteLine($"\tBienvenido {Environment.UserName} a su gestor de libro diario!\n");
+            LibroDiario.Iniciar();
+
+            do
+            {
+                Console.WriteLine(menuPrincipal);
+                opcionElegida = Console.ReadLine().ToUpper();
+
+                switch (opcionElegida)
+                {
+                    case "I":
+                        LibroDiario.IngresarAsiento();
+                        break;
+                    case "V":
+                        LibroDiario.VerAsientos();
+                        break;
+                    case "P":
+                        LibroDiario.VerPlanDeCuentas();
+                        break;
+                    default:
+                        Console.WriteLine($"'{opcionElegida}' no es una opción válida.\n");
+                        break;
+                }
+
+            } while (opcionElegida != "S");
+
+            LibroDiario.Guardar();
+
+            Console.WriteLine("Adios!");
+            Console.ReadKey();
         }
     }
 }
