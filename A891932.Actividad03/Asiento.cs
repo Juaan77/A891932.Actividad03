9767c07cae71d6a96faecf6a770f5425f009458d
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-06 16:04:29 -0300

    Agregado metodo para serializar

--- A891932.Actividad03/Asiento.cs
+++ A891932.Actividad03/Asiento.cs
@@ -15,5 +15,29 @@ namespace A891932.Actividad03
         double Debe { get; }
         double Haber { get; }
 
+        public Asiento(int numero, int codigoCuenta, double debe, double haber)
+        {
+            Numero = numero;
+            Fecha = DateTime.Today;
+            CodigoCuenta = codigoCuenta;
+            Debe = debe;
+            Haber = Haber;
+        }
+
+        public Asiento(string linea)
+        {
+            var datos = linea.Split('|');
+            Numero = int.Parse(datos[0]);
+            Fecha = DateTime.Parse(datos[1]);
+            CodigoCuenta = int.Parse(datos[2]);
+            Debe = double.Parse(datos[3]);
+            Haber = double.Parse(datos[4]);
+        }
+
+        public string Serializar()
+        {
+            return $"{Numero}|{Fecha}|{CodigoCuenta}|{Debe}|{Haber}";
+        }
+
     }
 }
