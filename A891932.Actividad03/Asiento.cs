3523429eb8ad80a25ba1089ebd6bc946a647ebf2
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-05 21:40:24 -0300

    Esqueleto basico del formato de un asiento

---
+++ A891932.Actividad03/Asiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891932.Actividad03
+{
+    class Asiento
+    {
+        // NroAsiento|Fecha|CodigoCuenta|Debe|Haber
+        int Numero { get; }
+        DateTime Fecha { get; }
+        int CodigoCuenta { get; }
+        double Debe { get; }
+        double Haber { get; }
+
+    }
+}
