7e9046ef32c9fa537ae5572eddc2e40a8cee3506
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-06 16:05:14 -0300

    Importar o crear un plan de cuentas ahora funciona

--- A891932.Actividad03/LibroDiario.cs
+++ A891932.Actividad03/LibroDiario.cs
@@ -10,10 +10,12 @@ namespace A891932.Actividad03
     class LibroDiario
     {
         static readonly Dictionary<int, Cuenta> Diario = new Dictionary<int, Cuenta>();
-        static string nombreDiario = "Diario.txt";
+        static string nombreDiario = "C:\\Diario.txt";
 
         static readonly Dictionary<int, Cuenta> Plan = new Dictionary<int, Cuenta>();
-        static string nombrePlan = "Plan de Cuentas.txt";
+        static string nombrePlan = $"C:/Users/{Environment.UserName}/documents/Plan de cuentas.txt";
+
+        static string ubicacionPlanDeCuentas;
 
         public static void AgregarCuenta()
         {
@@ -69,16 +71,20 @@ namespace A891932.Actividad03
                 }
             }
 
-            Console.WriteLine("----Presione una tecla para continuar----");
+            Console.WriteLine("----Presione una tecla para continuar----\n");
             Console.ReadKey();
         }
 
-        public static void ImportarPlanDeCuentas()
+        public static void PlanDeCuentas()
         {
             if (File.Exists(nombrePlan))
             {
-                using (var reader = new StreamReader(nombrePlan))     // Importa el txt (si este existe en la ubicacion por defecto)
+                ubicacionPlanDeCuentas = nombrePlan;
+
+                using (var reader = new StreamReader(ubicacionPlanDeCuentas))     // Importa el txt (si este existe en la ubicacion por defecto)
                 {
+                    reader.ReadLine(); // Solución muy sucia para ignorar la primera linea del txt.
+
                     while (!reader.EndOfStream)
                     {
                         var linea = reader.ReadLine();
@@ -87,12 +93,68 @@ namespace A891932.Actividad03
                     }
                 }
             }
+            else
+            {
+                bool ok = false;
+                string opcion;
+
+                do
+                {
+                    Console.WriteLine("No se ha encontrado un Plan de Cuentas existente. Desea (I)mportar uno existente o (C)rear uno nuevo?\n");                    
+                    opcion = Console.ReadLine().ToUpper();
+
+                    // Verifica que 'Plan de cuentas.txt' exista y lo importa.
+                    if (opcion == "I")
+                    {
+                        Console.WriteLine($"Ingrese la ubicación del archivo 'Plan de cuentas.txt' (Ej: 'C:/Plan de cuentas.txt' o 'C:/Users/{Environment.UserName}/documents/Plan de cuentas.txt'"); 
+                        ubicacionPlanDeCuentas = Console.ReadLine();
+
+                        if (!File.Exists(ubicacionPlanDeCuentas))
+                        {
+                            Console.WriteLine($"No se ha encontrado el archivo 'Plan de cuentas.txt' en {ubicacionPlanDeCuentas}\n");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            using (var reader = new StreamReader(ubicacionPlanDeCuentas))
+                            {
+                                reader.ReadLine(); // Solución muy sucia para ignorar la primera linea del txt.
+
+                                while (!reader.EndOfStream)
+                                {
+                                    var linea = reader.ReadLine();
+                                    var cuenta = new Cuenta(linea);
+                                    Plan.Add(cuenta.Codigo, cuenta);
+                                }
+                            }
+
+                            
+                            ok = true;
+                        }
+                    }else if(opcion == "C")     // Crea 'Plan de cuentas.txt' en la ubicacion 'C:\'
+                    {
+                        Console.WriteLine($"Se ha creado el archivo 'Plan de cuentas' en la ubicacion '{nombrePlan}'\n");
+                        using (StreamWriter writer = File.CreateText(nombrePlan))
+                        {
+                            writer.Write("Codigo|Nombre|Tipo");
+                        }
+
+                        ok = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"'{opcion} no es una opción válida.'\n");
+                        Console.ReadKey();
+                    }
+                } while (ok == false);
+            }
         }
 
         private static void GrabarPlan()
         {
-            using (var writer = new StreamWriter(nombrePlan, append: false))
+            using (var writer = new StreamWriter(ubicacionPlanDeCuentas))
             {
+                writer.WriteLine("Codigo|Nombre|Tipo");
                 foreach (var cuentas in Plan.Values)
                 {
                     var linea = cuentas.Serializar();
