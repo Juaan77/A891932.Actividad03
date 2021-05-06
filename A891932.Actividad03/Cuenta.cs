6bd8b5947b37a061c83d322fe99a78669b3dd2a6
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-05 21:41:43 -0300

    Se migro todo lo relacionado al plan de cuentas a LibroDiario

---
+++ A891932.Actividad03/Cuenta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891932.Actividad03
+{
+    public class Cuenta
+    {
+        public int Codigo { get; }
+        public string Nombre { get; }
+        public string Tipo { get; }
+
+
+        public Cuenta(int codigo, string nombre, string tipo)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Tipo = tipo;
+        }
+
+        public Cuenta(string linea)
+        {
+            var datos = linea.Split('|');
+            Codigo = int.Parse(datos[0]);
+            Nombre = datos[1];
+            Tipo = datos[2];
+        }
+
+        public string Serializar()
+        {
+            return $"{Codigo}|{Nombre}|{Tipo}";
+        }
+    }
+}
